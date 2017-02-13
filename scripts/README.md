40d3b02d5206b1ca8b0e64f6c134689440c81c20
Gijs Kant <gijs@thehyve.nl>
2017-02-13 10:22:18 +0100

    Update.

--- scripts/README.md
+++ scripts/README.md
@@ -6,8 +6,11 @@ Command to install dependencies:
 pip install -r requirements.txt
 ```
 
+If the `mcrl2` and `ltsmin` tools are not globally installed,
+edit `config.json` to set the path where they can be found.
+
 Command to generate LPS and PBES files for the experiments:
 ```
-../../scripts/experiments.py experiments.json prepare
+../../scripts/experiments.py config.json experiments.json prepare
 ```
 
--- scripts/install_mcrl2.sh
+++ scripts/install_mcrl2.sh
@@ -14,5 +14,6 @@ cd mcrl2-pmc
 
 cmake . -DMCRL2_ENABLE_GUI_TOOLS=OFF -DMCRL2_ENABLE_EXPERIMENTAL=ON -DCMAKE_INSTALL_PREFIX=$prefix
 make && make install
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${prefix}/lib/mcrl2/" > set_librarypath.sh
-chmod +x set_librarypath.sh
+cd ..
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${prefix}/lib/mcrl2/" > scripts/set_librarypath.sh
+chmod +x scripts/set_librarypath.sh
